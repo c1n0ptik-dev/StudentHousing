8354dcda26786bbd48c47d1eb05814c4751024a5
Darii <i553730@fontysict.nl>
2024-12-04 13:00:47 +0100

    login form

---
+++ StudentHouses/StudentHouses/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace StudentHouses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.showPassword = new System.Windows.Forms.CheckBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.Navy;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.loginButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.loginButton.Location = new System.Drawing.Point(170, 562);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(199, 49);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(127, 364);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(48, 20);
+            this.label.TabIndex = 5;
+            this.label.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 443);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Password";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.showPassword);
+            this.groupBox1.Controls.Add(this.passwordBox);
+            this.groupBox1.Controls.Add(this.loginBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label);
+            this.groupBox1.Controls.Add(this.loginButton);
+            this.groupBox1.Location = new System.Drawing.Point(365, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(523, 683);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(87, 47);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(355, 284);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // showPassword
+            // 
+            this.showPassword.AutoSize = true;
+            this.showPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.showPassword.Location = new System.Drawing.Point(131, 507);
+            this.showPassword.Name = "showPassword";
+            this.showPassword.Size = new System.Drawing.Size(156, 21);
+            this.showPassword.TabIndex = 9;
+            this.showPassword.Text = "Show the password";
+            this.showPassword.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.showPassword.UseVisualStyleBackColor = true;
+            this.showPassword.CheckedChanged += new System.EventHandler(this.showPassword_CheckedChanged);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.passwordBox.Location = new System.Drawing.Point(131, 466);
+            this.passwordBox.Multiline = true;
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(269, 35);
+            this.passwordBox.TabIndex = 8;
+            this.passwordBox.TextChanged += new System.EventHandler(this.passwordBox_TextChanged);
+            // 
+            // loginBox
+            // 
+            this.loginBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.loginBox.Location = new System.Drawing.Point(131, 387);
+            this.loginBox.Multiline = true;
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(269, 35);
+            this.loginBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(1254, 838);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.CheckBox showPassword;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ StudentHouses/StudentHouses/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace StudentHouses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+          
+            loginButton.FlatStyle = FlatStyle.Flat;
+            loginButton.FlatAppearance.BorderSize = 0;
+            loginButton.FlatAppearance.MouseDownBackColor = Color.Transparent;
+
+
+            passwordBox.PasswordChar = '*';
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string loginAdmin = loginBox.Text;
+            string passwordAdmin = passwordBox.Text;
+
+            if (loginAdmin == "Admin" && passwordAdmin == "pas123")
+            {
+                AdminForm admin = new AdminForm();
+                admin.Show();
+                this.Hide();
+            }
+            else if (loginAdmin == "Student" && passwordAdmin == "s123")
+            {
+                StudentForm student = new StudentForm();
+                student.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Access denied!");
+            }
+        }
+
+        private void passwordBox_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void showPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (showPassword.Checked)
+            {
+                passwordBox.PasswordChar = '\0';
+            }
+            else
+            {
+                passwordBox.PasswordChar = '*';
+            }
+        }
+    }
+}
